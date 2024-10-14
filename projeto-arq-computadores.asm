fb7fc684f93abefe2315506c873c8eb3268578d1
Rafaela Altheman <134940501+RafaAltheman@users.noreply.github.com>
2024-10-14 15:05:50 +0000

    Lógica do teclado matricial add

--- projeto-arq-computadores.asm
+++ projeto-arq-computadores.asm
@@ -1,360 +1,318 @@
 ; --- Mapeamento de Hardware (8051) ---
-    RS      equ     P1.3    ;Reg Select ligado em P1.3
-    EN      equ     P1.2    ;Enable ligado em P1.2
-
+RS      equ     P1.3    ; Reg Select ligado em P1.3
+EN      equ     P1.2    ; Enable ligado em P1.2
 
 org 0000h
-	LJMP START
+    LJMP START
 
 org 0030h
+
 ; Escritas do LCD
 BemVindo:
-	DB "Bem vindo!"
-  DB 00h ;Marca null no fim da String
+  DB "Bem vindo!"
+  DB 00h ; Marca null no fim da String
 Escolha:
-  DB "Escoha a bebida"
-  DB 00h ;Marca null no fim da String
+  DB "Escolha a bebida"
+  DB 00h ; Marca null no fim da String
 Agua:
   DB "1 - Agua"
-  DB 00h ;Marca null no fim da String
+  DB 00h ; Marca null no fim da String
 CocaCola:
   DB "2 - Coca Cola"
-  DB 00h ;Marca null no fim da String
+  DB 00h ; Marca null no fim da String
 Cha:
-  DB "3 - Ch� Mate"
-  DB 00h ;Marca null no fim da String
+  DB "3 - Chá Mate"
+  DB 00h ; Marca null no fim da String
 Suco:
   DB "4 - Suco"
-  DB 00h ;Marca null no fim da String
+  DB 00h ; Marca null no fim da String
 Digite:
   DB "Digite o numero"
-  DB 00h ;Marca null no fim da String
+  DB 00h ; Marca null no fim da String
 
-;MAIN
+; Mensagens de preparação
+PreparandoAgua:
+  DB "Preparando Agua..."
+  DB 00h
+PreparandoCocaCola:
+  DB "Preparando Coca Cola..."
+  DB 00h
+PreparandoCha:
+  DB "Preparando Chá Mate..."
+  DB 00h
+PreparandoSuco:
+  DB "Preparando Suco..."
+  DB 00h
+
+; MAIN
 org 0100h
 START:
-; put data in RAM
-	MOV 40H, #'#' 
-	MOV 41H, #'0'
-	MOV 42H, #'*'
-	MOV 43H, #'9'
-	MOV 44H, #'8'
-	MOV 45H, #'7'
-	MOV 46H, #'6'
-	MOV 47H, #'5'
-	MOV 48H, #'4'
-	MOV 49H, #'3'
-	MOV 4AH, #'2'
-	MOV 4BH, #'1'	  
-main:
-	; Escrever bem vindo no LCD
-	ACALL lcd_init
-	MOV A, #00h
-	ACALL posicionaCursor
-	MOV DPTR,#BemVindo
-	ACALL escreveStringROM
-	MOV A, #40h
-  ACALL posicionaCursor
-	MOV DPTR,#Escolha
-  ACALL escreveStringROM
-	ACALL clearDisplay
+    ; Inicializa o LCD e escreve mensagem inicial
+    ACALL lcd_init
+    MOV A, #00h
+    ACALL posicionaCursor
+    MOV DPTR, #BemVindo
+    ACALL escreveStringROM
+    MOV A, #40h
+    ACALL posicionaCursor
+    MOV DPTR, #Escolha
+    ACALL escreveStringROM
+    ACALL clearDisplay
 
-	; Passar qual numero eh de cada bebida
-	ACALL lcd_init
-	MOV A, #00h
-	ACALL posicionaCursor
-	MOV DPTR,#Agua
-	ACALL escreveStringROM
-	MOV A, #40h
-  	ACALL posicionaCursor
-	MOV DPTR,#CocaCola
-	ACALL escreveStringROM
-	ACALL clearDisplay
-	ACALL lcd_init
-	MOV A, #00h
-	ACALL posicionaCursor
-	MOV DPTR,#Cha
-	ACALL escreveStringROM
-	MOV A, #40h
-  	ACALL posicionaCursor
-	MOV DPTR,#Suco
-	ACALL escreveStringROM
-	ACALL clearDisplay
-	ACALL lcd_init
-	MOV A, #00h
-	ACALL posicionaCursor
-	MOV DPTR,#Digite
-	ACALL escreveStringROM
-	ACALL clearDisplay
-	
-	; receber a informa��o de qual bebida o user escolheu
-	ROTINA:
-	ACALL leituraTeclado
-	JNB F0, ROTINA   ;if F0 is clear, jump to ROTINA
-	MOV A, #07h
-	ACALL posicionaCursor	
-	MOV A, #40h
-	ADD A, R0
-	MOV R0, A
-	MOV A, @R0        
-	ACALL sendCharacter
-	CLR F0
-	JMP ROTINA
-	
-	SJMP $
+    ; Exibe as opções de bebidas
+    ACALL lcd_init
+    MOV A, #00h
+    ACALL posicionaCursor
+    MOV DPTR, #Agua
+    ACALL escreveStringROM
+    MOV A, #40h
+    ACALL posicionaCursor
+    MOV DPTR, #CocaCola
+    ACALL escreveStringROM
+    ACALL clearDisplay
+    ACALL lcd_init
+    MOV A, #00h
+    ACALL posicionaCursor
+    MOV DPTR, #Cha
+    ACALL escreveStringROM
+    MOV A, #40h
+    ACALL posicionaCursor
+    MOV DPTR, #Suco
+    ACALL escreveStringROM
+    ACALL clearDisplay
+    ACALL lcd_init
+    MOV A, #00h
+    ACALL posicionaCursor
+    MOV DPTR, #Digite
+    ACALL escreveStringROM
 
-; Fun��es do LCD
+    ; Loop principal para escolha da bebida
+ROTINA:
+    ACALL leituraTeclado      ; Ler o teclado matricial
+    JNB F0, ROTINA            ; Se nenhuma tecla foi pressionada, continua verificando
+
+    ; Verifica qual número foi pressionado
+    CJNE R0, #0Bh, check2     ; Se for '1', pula para Coca-Cola
+    MOV DPTR, #PreparandoAgua
+    ACALL exibePreparacao
+    SJMP ROTINA
+
+check2:
+    CJNE R0, #0Ah, check3     ; Se for '2', pula para Coca-Cola
+    MOV DPTR, #PreparandoCocaCola
+    ACALL exibePreparacao
+    SJMP ROTINA
+
+check3:
+    CJNE R0, #09h, check4     ; Se for '3', pula para Chá
+    MOV DPTR, #PreparandoCha
+    ACALL exibePreparacao
+    SJMP ROTINA
+
+check4:
+    CJNE R0, #08h, ROTINA     ; Se for '4', prepara Suco
+    MOV DPTR, #PreparandoSuco
+    ACALL exibePreparacao
+    SJMP ROTINA
+
+    SJMP $                   ; Loop infinito
+
+; Função para exibir mensagem de preparação
+exibePreparacao:
+    ACALL clearDisplay       ; Limpa o LCD
+    MOV A, #00h              ; Define a posição do cursor
+    ACALL posicionaCursor
+    ACALL escreveStringROM   ; Escreve a string que está em DPTR no LCD
+    RET
+
+; Funções do LCD
 escreveStringROM:
   MOV R1, #00h
-	; Inicia a escrita da String no Display LCD
+  ; Inicia a escrita da String no Display LCD
 loop:
   MOV A, R1
-	MOVC A,@A+DPTR 	 ;l� da mem�ria de programa
-	JZ finish2		; if A is 0, then end of data has been reached - jump out of loop
-	ACALL sendCharacter	; send data in A to LCD module
-	INC R1			; point to next piece of data
-   MOV A, R1
-	JMP loop		; repeat
+  MOVC A,@A+DPTR 	 ; lê da memória de programa
+  JZ finish2		 ; if A is 0, then end of data has been reached - jump out of loop
+  ACALL sendCharacter	 ; send data in A to LCD module
+  INC R1		 ; point to next piece of data
+  MOV A, R1
+  JMP loop		 ; repeat
 finish2:
-	RET
+  RET
 
-; initialise the display
-; see instruction set for details
 lcd_init:
-	CLR RS		; clear RS - indicates that instructions are being sent to the module
-
-; function set	
-	CLR P1.7		; |
-	CLR P1.6		; |
-	SETB P1.5		; |
-	CLR P1.4		; | high nibble set
-
-	SETB EN		; |
-	CLR EN		; | negative edge on E
-
-	CALL delay		; wait for BF to clear	
-					; function set sent for first time - tells module to go into 4-bit mode
-; Why is function set high nibble sent twice? See 4-bit operation on pages 39 and 42 of HD44780.pdf.
-
-	SETB EN		; |
-	CLR EN		; | negative edge on E
-					; same function set high nibble sent a second time
-
-	SETB P1.7		; low nibble set (only P1.7 needed to be changed)
-
-	SETB EN		; |
-	CLR EN		; | negative edge on E
-				; function set low nibble sent
-	CALL delay		; wait for BF to clear
-
-
-; entry mode set
-; set to increment with no shift
-	CLR P1.7		; |
-	CLR P1.6		; |
-	CLR P1.5		; |
-	CLR P1.4		; | high nibble set
-
-	SETB EN		; |
-	CLR EN		; | negative edge on E
-
-	SETB P1.6		; |
-	SETB P1.5		; |low nibble set
-
-	SETB EN		; |
-	CLR EN		; | negative edge on E
-
-	CALL delay		; wait for BF to clear
-
-
-; display on/off control
-; the display is turned on, the cursor is turned on and blinking is turned on
-	CLR P1.7		; |
-	CLR P1.6		; |
-	CLR P1.5		; |
-	CLR P1.4		; | high nibble set
-
-	SETB EN		; |
-	CLR EN		; | negative edge on E
-
-	SETB P1.7		; |
-	SETB P1.6		; |
-	SETB P1.5		; |
-	SETB P1.4		; | low nibble set
-
-	SETB EN		; |
-	CLR EN		; | negative edge on E
-
-	CALL delay		; wait for BF to clear
-	RET
+  ; Inicializa o LCD
+  CLR RS
+  ; Função set
+  CLR P1.7
+  CLR P1.6
+  SETB P1.5
+  CLR P1.4
+  SETB EN
+  CLR EN
+  CALL delay
+  SETB EN
+  CLR EN
+  SETB P1.7
+  SETB EN
+  CLR EN
+  CALL delay
+  ; Entry mode set
+  CLR P1.7
+  CLR P1.6
+  CLR P1.5
+  CLR P1.4
+  SETB EN
+  CLR EN
+  SETB P1.6
+  SETB P1.5
+  SETB EN
+  CLR EN
+  CALL delay
+  ; Display on/off control
+  CLR P1.7
+  CLR P1.6
+  CLR P1.5
+  CLR P1.4
+  SETB EN
+  CLR EN
+  SETB P1.7
+  SETB P1.6
+  SETB P1.5
+  SETB P1.4
+  SETB EN
+  CLR EN
+  CALL delay
+  RET
 
 sendCharacter:
-	SETB RS  		; setb RS - indicates that data is being sent to module
-	MOV C, ACC.7		; |
-	MOV P1.7, C			; |
-	MOV C, ACC.6		; |
-	MOV P1.6, C			; |
-	MOV C, ACC.5		; |
-	MOV P1.5, C			; |
-	MOV C, ACC.4		; |
-	MOV P1.4, C			; | high nibble set
+  SETB RS  		; Envia um caractere para o LCD
+  MOV C, ACC.7
+  MOV P1.7, C
+  MOV C, ACC.6
+  MOV P1.6, C
+  MOV C, ACC.5
+  MOV P1.5, C
+  MOV C, ACC.4
+  MOV P1.4, C
+  SETB EN
+  CLR EN
+  MOV C, ACC.3
+  MOV P1.7, C
+  MOV C, ACC.2
+  MOV P1.6, C
+  MOV C, ACC.1
+  MOV P1.5, C
+  MOV C, ACC.0
+  MOV P1.4, C
+  SETB EN
+  CLR EN
+  CALL delay
+  CALL delay
+  RET
 
-	SETB EN			; |
-	CLR EN			; | negative edge on E
-
-	MOV C, ACC.3		; |
-	MOV P1.7, C			; |
-	MOV C, ACC.2		; |
-	MOV P1.6, C			; |
-	MOV C, ACC.1		; |
-	MOV P1.5, C			; |
-	MOV C, ACC.0		; |
-	MOV P1.4, C			; | low nibble set
-
-	SETB EN			; |
-	CLR EN			; | negative edge on E
-
-	CALL delay			; wait for BF to clear
-	CALL delay			; wait for BF to clear
-	RET
-
-;Posiciona o cursor na linha e coluna desejada.
-;Escreva no Acumulador o valor de endere�o da linha e coluna.
-;|--------------------------------------------------------------------------------------|
-;|linha 1 | 00 | 01 | 02 | 03 | 04 |05 | 06 | 07 | 08 | 09 |0A | 0B | 0C | 0D | 0E | 0F |
-;|linha 2 | 40 | 41 | 42 | 43 | 44 |45 | 46 | 47 | 48 | 49 |4A | 4B | 4C | 4D | 4E | 4F |
-;|--------------------------------------------------------------------------------------|
 posicionaCursor:
-	CLR RS	
-	SETB P1.7		    ; |
-	MOV C, ACC.6		; |
-	MOV P1.6, C			; |
-	MOV C, ACC.5		; |
-	MOV P1.5, C			; |
-	MOV C, ACC.4		; |
-	MOV P1.4, C			; | high nibble set
+  CLR RS	
+  SETB P1.7
+  MOV C, ACC.6
+  MOV P1.6, C
+  MOV C, ACC.5
+  MOV P1.5, C
+  MOV C, ACC.4
+  MOV P1.4, C
+  SETB EN
+  CLR EN
+  MOV C, ACC.3
+  MOV P1.7, C
+  MOV C, ACC.2
+  MOV P1.6, C
+  MOV C, ACC.1
+  MOV P1.5, C
+  MOV C, ACC.0
+  MOV P1.4, C
+  SETB EN
+  CLR EN
+  CALL delay
+  CALL delay
+  RET
 
-	SETB EN			; |
-	CLR EN			; | negative edge on E
-
-	MOV C, ACC.3		; |
-	MOV P1.7, C			; |
-	MOV C, ACC.2		; |
-	MOV P1.6, C			; |
-	MOV C, ACC.1		; |
-	MOV P1.5, C			; |
-	MOV C, ACC.0		; |
-	MOV P1.4, C			; | low nibble set
-
-	SETB EN			; |
-	CLR EN			; | negative edge on E
-
-	CALL delay			; wait for BF to clear
-	CALL delay			; wait for BF to clear
-	RET
-
-
-;Retorna o cursor para primeira posi��o sem limpar o display
-retornaCursor:
-	CLR RS	
-	CLR P1.7		; |
-	CLR P1.6		; |
-	CLR P1.5		; |
-	CLR P1.4		; | high nibble set
-
-	SETB EN		; |
-	CLR EN		; | negative edge on E
-
-	CLR P1.7		; |
-	CLR P1.6		; |
-	SETB P1.5		; |
-	SETB P1.4		; | low nibble set
-
-	SETB EN		; |
-	CLR EN		; | negative edge on E
-
-	CALL delay		; wait for BF to clear
-	RET
-
-;Limpa o display
 clearDisplay:
-	CLR RS	
-	CLR P1.7		; |
-	CLR P1.6		; |
-	CLR P1.5		; |
-	CLR P1.4		; | high nibble set
-
-	SETB EN		; |
-	CLR EN		; | negative edge on E
-
-	CLR P1.7		; |
-	CLR P1.6		; |
-	CLR P1.5		; |
-	SETB P1.4		; | low nibble set
-
-	SETB EN		; |
-	CLR EN		; | negative edge on E
-
-	MOV R6, #40
-	rotC:
-	CALL delay		; wait for BF to clear
-	DJNZ R6, rotC
-	RET
-
+  CLR RS	
+  CLR P1.7
+  CLR P1.6
+  CLR P1.5
+  CLR P1.4
+  SETB EN
+  CLR EN
+  CLR P1.7
+  CLR P1.6
+  CLR P1.5
+  SETB P1.4
+  SETB EN
+  CLR EN
+  MOV R6, #40
+rotC:
+  CALL delay
+  DJNZ R6, rotC
+  RET
 
 delay:
-	MOV R0, #50
-	DJNZ R0, $
-	RET
+  MOV R0, #50
+  DJNZ R0, $
+  RET
 
-; Fun��es do teclado
-;	+----+----+----+
-;	| 11 | 10 |  9 |	row3
-;	+----+----+----+
-;	|  8 |  7 |  6 |	row2
-;	+----+----|----+
-;	|  5 |  4 |  3 |	row1
-;	+----+----+----+
-;	|  2 |  1 |  0 |	row0
-;	+----+----+----+
-;	 col2 col1 col0
+; Funções do teclado matricial
 leituraTeclado:
-	MOV R0, #0			; clear R0 - the first key is key0 scan row0
-	MOV P0, #0FFh	
-	CLR P0.0			; clear row0
-	CALL colScan		; call column-scan subroutine
-	JB F0, finish		; | if F0 is set, jump to end of program 
-						; | (because the pressed key was found and its number is in  R0)
-	; scan row1
-	SETB P0.0			; set row0
-	CLR P0.1			; clear row1
-	CALL colScan		; call column-scan subroutine
-	JB F0, finish		; | if F0 is set, jump to end of program 
-						; | (because the pressed key was found and its number is in  R0)
-	; scan row2
-	SETB P0.1			; set row1
-	CLR P0.2			; clear row2
-	CALL colScan		; call column-scan subroutine
-	JB F0, finish		; | if F0 is set, jump to end of program 
-						; | (because the pressed key was found and its number is in  R0)
-	; scan row3
-	SETB P0.2			; set row2
-	CLR P0.3			; clear row3
-	CALL colScan		; call column-scan subroutine
-	JB F0, finish		; | if F0 is set, jump to end of program 
-						; | (because the pressed key was found and its number is in  R0)
-finish:
-	RET
+    MOV P0, #0F0h        ; Configura as linhas como saída (P0.4 a P0.7)
+    NOP                  ; Pequeno atraso
+    MOV A, P0            ; Lê as colunas (P0.0 a P0.3)
+    ANL A, #0F0h         ; Verifica se alguma tecla foi pressionada
+    JZ semTecla          ; Se não houver tecla pressionada, pula para semTecla
+    
+    MOV P0, #0FEh        ; Ativa a linha 1
+    MOV A, P0
+    ANL A, #0F0h
+    CJNE A, #0F0h, tecla1
+    
+    MOV P0, #0FDh        ; Ativa a linha 2
+    MOV A, P0
+    ANL A, #0F0h
+    CJNE A, #0F0h, tecla2
+    
+    MOV P0, #0FBh        ; Ativa a linha 3
+    MOV A, P0
+    ANL A, #0F0h
+    CJNE A, #0F0h, tecla3
+    
+    MOV P0, #0F7h        ; Ativa a linha 4
+    MOV A, P0
+    ANL A, #0F0h
+    CJNE A, #0F0h, tecla4
 
-; column-scan subroutine
-colScan:
-	JNB P0.4, gotKey	; if col0 is cleared - key found
-	INC R0				; otherwise move to next key
-	JNB P0.5, gotKey	; if col1 is cleared - key found
-	INC R0				; otherwise move to next key
-	JNB P0.6, gotKey	; if col2 is cleared - key found
-	INC R0				; otherwise move to next key
-	RET					; return from subroutine - key not found
-gotKey:
-	SETB F0				; key found - set F0
-	RET					; and return from subroutine
+    SJMP semTecla        ; Sem tecla pressionada
+
+tecla1:
+    MOV R0, #0Bh         ; Tecla '1'
+    SETB F0              ; Indica que uma tecla foi pressionada
+    RET
+
+tecla2:
+    MOV R0, #0Ah         ; Tecla '2'
+    SETB F0
+    RET
+
+tecla3:
+    MOV R0, #09h         ; Tecla '3'
+    SETB F0
+    RET
+
+tecla4:
+    MOV R0, #08h         ; Tecla '4'
+    SETB F0
+    RET
+
+semTecla:
+    CLR F0               ; Nenhuma tecla pressionada
+    RET
+
+end
